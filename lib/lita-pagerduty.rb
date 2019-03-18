1551efeb0788389cc20fad1cec4a63ef8ae36747
Noel Cower <ncower@gmail.com>
2019-03-18 16:23:06 -0700

    Fix help text not pulling localized strings
    - Use t() to load the localized strings.
    - Load localized strings before setting up routes, since otherwise the
      localized strings aren't available at the time the class is created.

--- lib/lita-pagerduty.rb
+++ lib/lita-pagerduty.rb
@@ -1,14 +1,16 @@
 require 'lita'
+
+Lita.load_locales Dir[File.expand_path(
+  File.join('..', '..', 'locales', '*.yml'), __FILE__
+)]
+
 require 'exceptions'
 require 'pagerduty'
 require 'store'
 require 'lita/handlers/pagerduty'
 require 'lita/commands/base'
-Dir[File.join(__dir__, 'lita', 'commands', '*.rb')].each { |file| require file }
 
-Lita.load_locales Dir[File.expand_path(
-  File.join('..', '..', 'locales', '*.yml'), __FILE__
-)]
+Dir[File.join(__dir__, 'lita', 'commands', '*.rb')].each { |file| require file }
 
 Lita::Handlers::Pagerduty.template_root File.expand_path(
   File.join('..', '..', 'templates'), __FILE__
